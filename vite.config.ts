b1bd3b0f611d8c28a407be9359a331721feef594
Caio Tracera <ctrcra@gmail.com>
2025-10-31 10:44:11 +0000

    ✨ feat: initial commit

---
+++ vite.config.ts
@@ -0,0 +1,16 @@
+import path from "node:path";
+import react from "@vitejs/plugin-react-swc";
+import { defineConfig } from "vitest/config";
+
+// https://vite.dev/config/
+export default defineConfig({
+  plugins: [react()],
+  resolve: {
+    alias: {
+      "@": path.resolve(__dirname, "src"),
+    },
+  },
+  test: {
+    environment: "happy-dom",
+  },
+});
